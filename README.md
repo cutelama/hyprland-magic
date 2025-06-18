38a9a7e7502a7d6febe3a753100d5cdc43d1c907
armenin <biyoar909@proton.me>
2025-06-18 21:42:06 +0500

    hyper config update

--- README.md
+++ README.md
@@ -1,5 +1,14 @@
+## dependencies
+- wal
+- swww
+- rofi-calc
+- 
+- gdk theme: adw-gdk3-dark
+- 
 ## Screenshots
 
-![изображение](https://github.com/user-attachments/assets/892788f0-6176-42fe-b09b-ab7eaffd8cea)
 ![изображение](https://github.com/user-attachments/assets/c4d159c6-c8a7-4160-976e-9b8a064c8e3a)
 ![изображение](https://github.com/user-attachments/assets/6287afd3-97df-4688-bf2e-f0ac0a33d184)
+![img_1.png](img_1.png)
+![img_3.png](img_3.png)
+![img_4.png](img_4.png)
--- hypr/syncBorderColor.sh
+++ hypr/syncBorderColor.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-CONFIG_INI="$HOME/.config/waypaper/config.ini" # waypaper config
-WALLPAPER_PATH=$(grep '^wallpaper =' "$CONFIG_INI" | cut -d '=' -f2- | tr -d ' ' | xargs -I {} echo {})
-WALLPAPER_PATH=$(eval echo "$WALLPAPER_PATH")
+WALLPAPER=$(find "$HOME/Docs/Pics/Wallpapers/" -type f | shuf -n 1)
+ANGLE=$((RANDOM % 360))
+TRANSITION="--transition-type wave --transition-angle $ANGLE --transition-fps 120"
+
+swww img "$WALLPAPER" $TRANSITION
+
+sleep 2
 
 wal -c
-wal -i "$WALLPAPER_PATH"
+wal -i "$WALLPAPER"
 
 COLORS_SOURCE="$HOME/.cache/wal/colors"
 CONFIG_FILE="$HOME/.config/hypr/colors.conf"
@@ -25,9 +29,3 @@ mv -f "$TMP_FILE" "$CONFIG_FILE" # bypass text of hyprland_config_error
 echo "Colors successfully updated in $CONFIG_FILE"
 
 
-#color1=$(echo "${colors[1]#\#}" | tr '[:upper:]' '[:lower:]')ff
-#color6=$(echo "${colors[6]#\#}" | tr '[:upper:]' '[:lower:]')ff
-#color7=$(echo "${colors[7]#\#}" | tr '[:upper:]' '[:lower:]')ff
-#
-#hyprctl keyword general:col.active_border "rgba($color6) rgba($color7) 45deg"
-#hyprctl keyword general:col.inactive_border "rgba($color1)"
